38f93cd695fc98726e7dda99344a0c2cc605209e
gpedrosilva13 <gpedrosilva@professor.educacao.sp.gov.br>
2024-06-03 14:50:25 -0300

    Add files via upload

---
+++ sistema_20_leds_com_variaveis_1.ino
@@ -0,0 +1,27 @@
+/*
+byte pino=0;
+byte led=0;
+void setup()
+{
+  while(pino<20)
+  { 
+    pinMode(pino,OUTPUT);
+    pino=pino+1;
+}
+}
+void loop()
+{
+  if(led<20)
+  {
+  
+  digitalWrite(led,HIGH);
+  delay(1000);
+  led++;
+
+    
+  }
+  
+
+  
+  }
+*/
